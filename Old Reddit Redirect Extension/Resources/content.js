422e54ca38170223572fa8c14f628dbb19c99e13
Eric Ventor <ericventor97@gmail.com>
2023-07-09 16:32:20 -0700

    init

--- Old Reddit Redirect Extension/Resources/content.js
+++ Old Reddit Redirect Extension/Resources/content.js
@@ -1,6 +1,6 @@
 if (window.location.href.startsWith('https://www.reddit.com/')) {
     // Replace 'www.reddit.com' with 'old.reddit.com' in the URL
-    var newURL = window.location.href.replace('www.reddit.com', 'old.reddit.com');
+    var newURL = window.location.href.replace('https://www.reddit.com', 'old.reddit.com');
 
     // Navigate to the new URL
     window.location.href = newURL;
