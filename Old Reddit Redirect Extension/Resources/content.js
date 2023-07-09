df1832f412b5584a44854cc8c31e7c48b3d85289
Eric Ventor <ericventor97@gmail.com>
2023-07-09 16:23:06 -0700

    Initial Commit

---
+++ Old Reddit Redirect Extension/Resources/content.js
@@ -0,0 +1,15 @@
+if (window.location.href.startsWith('https://www.reddit.com/')) {
+    // Replace 'www.reddit.com' with 'old.reddit.com' in the URL
+    var newURL = window.location.href.replace('www.reddit.com', 'old.reddit.com');
+
+    // Navigate to the new URL
+    window.location.href = newURL;
+}
+
+browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
+    console.log("Received response: ", response);
+});
+
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("Received request: ", request);
+});
---
+++ Old Reddit Redirect/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Old Reddit Redirect
+//
+//  Created by Eric Ventor on 7/9/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
